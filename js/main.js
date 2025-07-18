4bfe2ea203c1c320bcccc21d7ee37d0b6d50b543
Paul Hwang <hwpaul1007@gmail.com>
2025-07-17 17:33:44 -0700

    Implement Header Functionality and Styling

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
+function highlightCurrentPageLink() {
+  const currentPath = window.location.pathname;
+  const headerLinks = document.querySelectorAll(".header-link");
+
+  headerLinks.forEach((link) => {
+    const linkPath = new URL(link.href).pathname;
+
+    if (
+      linkPath === currentPath ||
+      (currentPath === "/" && linkPath.includes("index.html"))
+    ) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+}
+
 // Load Custom Components
 function loadHeader() {
   fetch("/components/header.html")
     .then((res) => res.text())
     .then((data) => {
       document.getElementById("header-placeholder").innerHTML = data;
+      highlightCurrentPageLink();
     });
 }
