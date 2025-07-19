ddb30c41fc2bf39e5566060c1d217ec5fa931176
Paul Hwang <hwpaul1007@gmail.com>
2025-07-19 15:56:44 -0700

    Footer

--- js/main.js
+++ js/main.js
@@ -27,4 +27,12 @@ function loadHeader() {
     });
 }
 
+function loadFooter() {
+  fetch("/components/footer.html")
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById("footer-placeholder").innerHTML = data;
+    });
+}
+
 // Horizontal Image Loop
